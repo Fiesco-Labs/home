d2d801505e9d980edd8ae1f774399006b2f54296
Ishan Avishka <ishanavishka14@gmail.com>
2020-05-19 21:49:05 +0530

    fiesco labs official website

--- js/demo.js
+++ js/demo.js
@@ -26,11 +26,11 @@ WebFontConfig = {
 $.contactButtons({
   effect  : 'slide-on-scroll',
   buttons : {
-    'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/pages/mycompany', extras: 'target="_blank"' },
-    'linkedin':   { class: 'linkedin', use: true, link: 'https://www.linkedin.com/company/mycompany' },
+    'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/Fiesco-Labs-113254070391191', extras: 'target="_blank"' },
+    'linkedin':   { class: 'linkedin', use: true, link: 'https://www.linkedin.com/in/fiesco-labs-6653371aa/' },
     'google':     { class: 'gplus',    use: true, link: 'https://plus.google.com/myidongoogle' },
     'mybutton':   { class: 'git',      use: true, link: 'http://github.com', icon: 'github', title: 'My title for the button' },
-    'phone':      { class: 'phone separated',    use: true, link: '+000' },
-    'email':      { class: 'email',    use: true, link: 'test@web.com' }
+    'phone':      { class: 'phone separated',    use: true, link: '+94715507846' },
+    'email':      { class: 'email',    use: true, link: 'fiesco.labs@gmail.com' }
   }
 });
